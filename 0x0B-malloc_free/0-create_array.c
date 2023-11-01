fde4394ac5a177d2d8b3e86a854fc38c4cd24bbb
0x8anon <avdbergbusiness@skiff.com>
2023-11-01 01:28:13 +0100

    test2

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,24 +4,17 @@
  * 
  * @c : 
 */
-char *create_array(unsigned int size, char c)
-{
-
-if (size == 0)
-{
-    return NULL;
+char *create_array(unsigned int size, char c) {
+char *array;    
+if (size == 0) {
+return NULL;
 }
-char *array = (char *)malloc(size * sizeof(char));
-
-if (array = NULL)
-{
-    return NULL;
+array = (char *)malloc(size * sizeof(char));
+if (array == NULL) {
+return NULL;
 }
-
-for (unsigned int i = 0; i < size; i++)
-{
+for (unsigned int i = 0; i < size; i++) {
 array[i] = c;
 }
-
 return array;
 }
