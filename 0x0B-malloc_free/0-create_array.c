4f79bd6262ef8b191735ebce549e7374f355ad57
0x8Anon <147836505+0x8Anon@users.noreply.github.com>
2023-11-01 01:21:03 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,24 +4,21 @@
  * 
  * @c : 
 */
-char *create_array(unsigned int size, char c)
-{
+char *create_array(unsigned int size, char c) {
+    char *array;
+    
+    if (size == 0) {
+        return NULL; 
+    }
 
-if (size == 0)
-{
-    return NULL;
-}
-char *array = (char *)malloc(size * sizeof(char));
+    array = (char *)malloc(size * sizeof(char)); 
+    if (array == NULL) {
+        return NULL; 
+    }
 
-if (array = NULL)
-{
-    return NULL;
-}
+    for (unsigned int i = 0; i < size; i++) {
+        array[i] = c; 
+    }
 
-for (unsigned int i = 0; i < size; i++)
-{
-array[i] = c;
-}
-
-return array;
+    return array; 
 }
