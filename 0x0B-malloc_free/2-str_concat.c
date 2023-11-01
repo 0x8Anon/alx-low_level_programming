d1b1f10e294cc5b593f8bf66bfad55f7fd634592
0x8anon <avdbergbusiness@skiff.com>
2023-11-02 00:05:16 +0100

    2-str_concat.c betty fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,10 +3,11 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
- *
+ * str_concat - main
+ * Description: adds 2 pointers
+ * Return: result
+ * @s1: read
+ * @s2: read
  */
 
 char *str_concat(char *s1, char *s2)
@@ -24,29 +25,29 @@ if (s2 == NULL)
 s2 = "";
 }
 length1 = 0;
-while(s1[length1] != '\0')
+while (s1[length1] != '\0')
 {
 length1++;
 }
 length2 = 0;
-while(s2[length2] != '\0')
+while (s2[length2] != '\0')
 {
 length2++;
 }
 result = (char *)malloc(length1 + length2 + 1);
-if(result == NULL)
+if (result == NULL)
 {
-return NULL;
+return (NULL);
 }
-for(i = 0; i < length1; i++)
+for (i = 0; i < length1; i++)
 {
 result[i] = s1[i];
 }
 j = 0;
-for(j = 0; j < length2; j++)
+for (j = 0; j < length2; j++)
 {
 result[length1 + j] = s2[j];
 }
 result[length1 + length2] = '\0';
-return result;		
+return (result);
 }
