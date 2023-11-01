1e5aa51a1492a22b97d5990fa9927492e2e195b0
0x8anon <avdbergbusiness@skiff.com>
2023-11-02 00:25:08 +0100

    3-alloc_grid.c fix loop through height

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,6 +20,8 @@ if (grid == NULL)
 {
 return NULL;
 }
+for (i = 0; i < height; i++)
+{
 grid[i] = (int *)malloc(width * sizeof(int));
 if (grid[i] == NULL)
 {
@@ -34,5 +36,6 @@ for (j = 0; j < width; j++)
 {
 grid[i][j] = 0;
 }
-return grid;
+}
+return (grid);
 }
