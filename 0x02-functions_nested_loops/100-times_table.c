db1b5d91008856305e881a5fde57bcb036bb17d7
0x8anon <avdbergbusiness@skiff.com>
2023-11-02 12:38:24 +0100

    100-times_tablebetty code style

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * print_times_table - Entry point
+ * Description: prints time tables
+ * @n: value read in
  *
  */
 
@@ -17,8 +17,7 @@ if (n < 0 || n > 15)
 {
 return;
 }
-
-for ( i = 0; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
@@ -27,12 +26,11 @@ if (j == 0)
 {
 printf("%d", result);
 }
-else 
+else
 {
 printf(",%4d", result);
 }
-
-if ( j < n )
+if (j < n)
 {
 printf(" ");
 }
