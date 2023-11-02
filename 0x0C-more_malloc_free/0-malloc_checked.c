3d8b1e5ae9e0b8c3eb13184458cc5983230cae9e
0x8anon <avdbergbusiness@skiff.com>
2023-11-02 20:56:53 +0100

    0-malloc_checked.c fix

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * malloc_checked - Entry point
+ * Description: reads in b and then return
+ * Return: pointer
+ * @b: being read in
+ *
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b);
+
+if (ptr == NULL)
+{
+fprintf(stderr, "Memory allocation failed\n");
+exit(98);
+}
+
+return (ptr);
+}
