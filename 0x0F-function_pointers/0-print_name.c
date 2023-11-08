126a5954f032aea66d99269efc90f6bf61de9abb
0x8anon <avdbergbusiness@skiff.com>
2023-11-08 21:26:14 +0200

    0-print_name.c betty fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
-#include "main.h"
+#include "function_pointers.h"
+/**
+ * print_name - Entry point
+ * Description: printing name
+ * @f: read in
+ * @name: being printed
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+f(name);
+
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
-#ifndef MAIN
-#define MAIN
-
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
 #endif
 
