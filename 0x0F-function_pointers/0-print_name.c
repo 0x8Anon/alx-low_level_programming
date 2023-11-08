a0a0a1b2ff368b06d2fe70f560659afa8651f888
0x8anon <avdbergbusiness@skiff.com>
2023-11-08 21:37:44 +0200

    0-print_name.c NULL Byte fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stddef.h>
 /**
  * print_name - Entry point
  * Description: printing name
@@ -7,5 +8,8 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+if (name != NULL && f != NULL)
+{
 f(name);
+}
 }
