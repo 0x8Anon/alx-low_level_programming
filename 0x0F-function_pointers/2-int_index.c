c4f80b9e6c49719f0a20d09536bf3206f137e11c
0x8anon <avdbergbusiness@skiff.com>
2023-11-09 20:49:03 +0200

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,10 +1,12 @@
 #include "function_pointers.h"
 
 /**
- *
- *
- *
- *
+ * int_index - Entry point
+ * Description: searches for integer
+ * Return: 0 if false -1 if positive
+ * @size: read in
+ * @array: read in
+ * @cmp: read in
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
@@ -12,14 +14,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 int i;
 if (size <= 0 || array == NULL || cmp == NULL)
 {
-return -1;
+return (-1);
 }
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]) != 0)
 {
-return i;
+return (i);
 }
 }
-return -1;
+return (-1);
 }
